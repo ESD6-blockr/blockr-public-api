a7ca225187d6106e8aae96270b9c73814a5e21aa
Sven <s_devries@live.nl>
2019-04-17 11:12:29 +0200

    (BLO-27)-public-api: Transaction + tslint correct

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,10 @@
-import * as cors from "cors";
-import * as compression from "compression";
 import * as parser from "body-parser";
-  
-export default [cors({ credentials: true, origin: true }), parser.urlencoded({ extended: true }), parser.json(), compression()];
+import * as compression from "compression";
+import * as cors from "cors";
+
+export default [
+    cors({ credentials: true, origin: true }),
+    parser.urlencoded({ extended: true }),
+    parser.json(), 
+    compression(),
+];
