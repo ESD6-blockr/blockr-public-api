2d3addaa45928aa99d5494a00e2776b700fca7db
Sven <s_devries@live.nl>
2019-05-28 10:43:59 +0200

    BLO-72 made project injectable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { App } from "./app";
-import DIContainer from "./injection/container";
-import { AbstractRouter } from "./routers/abstractRouter";
 
 const PORT = 3000;
-const allRouters: AbstractRouter[] = DIContainer.getAll<AbstractRouter>("Routers");
 
-new App(allRouters, PORT).start();
+new App(PORT).start();
