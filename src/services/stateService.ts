17ffe399ae2dafb8433dfd12830fe89ad67ece16
Roel van den Boom <roelvandenboom@inogo.nl>
2019-06-17 11:16:28 +0200

    Fix retrieval of states

--- src/services/stateService.ts
+++ src/services/stateService.ts
@@ -15,7 +15,7 @@ export class StateService {
 
     public async getStateByPublicKey(request: Request, response: Response, next: NextFunction): Promise<void> {
         try {
-            logger.info(JSON.stringify(request.params.publicKey));
+            logger.info(`Getting state by publicKey: ${request.params.publicKey}.`);
             response.send(await this.dataAccessLayer.getStateAsync(request.params.publicKey));
             next();
         } catch (error) {
